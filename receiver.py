44a28bc755e85f610bafe38d473eff6937ec7de9
agg-kantas <agg.kantas@gmail.com>
2026-09-27 00:58:25 +0300

    Added a server-side program that opens a socket and listens for a connection

---
+++ receiver.py
@@ -0,0 +1,12 @@
+import socket
+import json
+import time
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1) # Socket Option Reuse Address
+s.bind(("",8080)) # "" IP address just means the receiver accepts any network interface the machine host has, better than using the actual IP that can change for multiple reasons
+s.listen(5)
+while True:
+    connection, client_address = s.accept()
+    print(connection,client_address)
+
+
